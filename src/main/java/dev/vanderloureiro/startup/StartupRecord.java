efff41a4486d7086e3beab47f63ae2a3de341db6
Vanderlei Loureiro <vanderloureiroleite@gmail.com>
2024-09-10 19:49:35 -0300

    confguração de transacoes do JPA

--- src/main/java/dev/vanderloureiro/startup/StartupRecord.java
+++ src/main/java/dev/vanderloureiro/startup/StartupRecord.java
@@ -8,9 +8,17 @@ import jakarta.persistence.Table;
 import java.time.LocalDate;
 
 @Entity
-@Table(name = "startup_record")
+@Table(name = "startup_records")
 public class StartupRecord extends PanacheEntity {
 
     @Column(name = "last_startup", nullable = false)
     LocalDate lastStartup;
+
+    public void updateRecord() {
+        this.lastStartup = LocalDate.now();
+    }
+
+    public boolean isPendingShipment() {
+        return this.lastStartup.isBefore(LocalDate.now());
+    }
 }
