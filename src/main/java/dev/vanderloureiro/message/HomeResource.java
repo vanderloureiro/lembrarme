2c2ae7fae429435cacbdb319c503ee9136527667
Vanderlei Loureiro <vanderloureiroleite@gmail.com>
2024-11-07 16:32:22 -0300

    escopo de endpoint create

--- src/main/java/dev/vanderloureiro/message/HomeResource.java
+++ src/main/java/dev/vanderloureiro/message/HomeResource.java
@@ -3,11 +3,18 @@ package dev.vanderloureiro.message;
 import io.quarkus.qute.Template;
 import io.quarkus.qute.TemplateInstance;
 
+import jakarta.ws.rs.BeanParam;
+import jakarta.ws.rs.Consumes;
+import jakarta.ws.rs.FormParam;
 import jakarta.ws.rs.GET;
+import jakarta.ws.rs.POST;
 import jakarta.ws.rs.Path;
 import jakarta.ws.rs.Produces;
 import jakarta.ws.rs.QueryParam;
 import jakarta.ws.rs.core.MediaType;
+import org.jboss.resteasy.reactive.RestForm;
+
+import java.util.Map;
 
 import static java.util.Objects.requireNonNull;
 
@@ -22,8 +29,22 @@ public class HomeResource {
 
     @GET
     @Produces(MediaType.TEXT_HTML)
-    public TemplateInstance get(@QueryParam("name") String name) {
-        return page.data("name", name);
+    public TemplateInstance get() {
+
+        return page.instance();
+    }
+
+    @POST
+    @Path("/create")
+    @Produces(MediaType.TEXT_HTML)
+    @Consumes(MediaType.APPLICATION_FORM_URLENCODED)
+    public TemplateInstance post(
+            @FormParam("body") String body,
+            @FormParam("email") String email,
+            @FormParam("date") String date,
+            @FormParam("recurrence") String recurrence) {
+
+        return page.instance();
     }
 
 }
